16b20785a2f6e4e4e9aa17dbad174193ddb10f8a
Leandro Almeida <leo.leandro.ars@gmail.com>
2024-06-11 22:10:42 -0300

    Trying to deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/loginJWT/frontend",
+  base: "/loginJWT/",
   server: {
     watch: {
       usePolling: true,
